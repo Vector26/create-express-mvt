581b44006888f546c5aba7eb89e5159d88ba90ed
vector26 <mrdev2604@gmail.com>
2021-10-13 01:52:14 +0530

    admin initial

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app=express.Router();
-const req_mods={
-    "default_app":"get_started"
-};
-// Import your modules here
-// {"NAME_OF_APP":"ROUTE_TITLE",....}
+const req_mods=require("./app_register.json");
 function main(){var e=[],r=0;Object.keys(req_mods).forEach(s=>{e.push(require(`./${s}/main`)),app.use(`/${req_mods[s]}`,e[r].routes),r+=1})}
 main();
 module.exports=app;
